cf92ab582ebfd40ec242685afc0281dad278aad0
Dangyron <78tuerpocv01@gmail.com>
2024-05-13 13:54:15 +0200

    Fixed some typos in db

---
+++ ChessServer.Data/Migrations/20240513112146_FixedSomeTypos.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace ChessServer.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class FixedSomeTypos : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "Moves",
+                table: "Games",
+                newName: "Pgn");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "Pgn",
+                table: "Games",
+                newName: "Moves");
+        }
+    }
+}
